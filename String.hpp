a91206d83396989ebab36e753df457a2886c5e5c
Mateo Tutic <mateo@tutic.ch>
2019-03-14 15:03:49 +0100

    Base de la classe string

--- String.hpp
+++ String.hpp
@@ -3,4 +3,14 @@
 
 #include <stdio.h>
 
+/**
+ * Permet d'implémenter une chaîne de caractères
+ */
+class String {
+public:
+
+private:
+   char* value;   // Valeur de la chaîne de caractères
+};
+
 #endif
